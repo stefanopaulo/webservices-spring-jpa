4f1fdbcdcfccd1c20832cadc72350d15f45aee2a
stefanotrab <stefano.souza@hapvida.com.br>
2021-02-15 11:34:19 -0300

    criando projeto

---
+++ src/main/java/com/educandoweb/webservices/WebservicesSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSpringJpaApplication.class, args);
+	}
+
+}
