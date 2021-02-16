63f21b2ca88b9769b1f21aeca25a2286267e97df
stefanotrab <stefano.souza@hapvida.com.br>
2021-02-16 13:44:33 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- src/main/java/com/educandoweb/webservices/entities/User.java
+++ src/main/java/com/educandoweb/webservices/entities/User.java
@@ -1,23 +1,32 @@
 package com.educandoweb.webservices.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 	
@@ -67,6 +76,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
